5b82b7279eeb00ecf088f4061d695e52afd9cc28
Etienne Springer <esp8999@gmail.com>
2023-12-30 01:59:59 +0100

    update .pkglist

---
+++ .pkglist/update_pkglist.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Saves lists of packages to three different files
+# aur: explicitly installed aur packages (i.e. with yay -S <package>)
+# pacman: explicitly installed arch packages (i.e. with pacman -S[yu] <package>
+# pacman-all: all packages and dependencies
+pacman -Qqem > "$HOME/.pkglist/aur"
+pacman -Qqen > "$HOME/.pkglist/pacman"
+pacman -Qqn  > "$HOME/.pkglist/pacman-all"
+
+# commits them to dotfiles
+
+dotfiles add $HOME/.pkglist/*
+dotfiles commit -m "Backup list of installed packages $(date +%Y-%m-%d_%H:%M)"
